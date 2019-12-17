8e34dfb67da89880bed841ccd8fd39369ea188cc
doriorio <itsdori@gmail.com>
2019-12-16 16:18:05 -0800

    added Use model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Plant
+from .models import Plant, Use
 
 
 admin.site.register(Plant)
+admin.site.register(Use)
--- main_app/models.py
+++ main_app/models.py
@@ -8,10 +8,30 @@ class Plant(models.Model):
     color = models.CharField(max_length=140)
     native_location = models.CharField(max_length=140)
     description = models.CharField(max_length=480, default="")
-    isHousePlant = models.BooleanField
+    
 
     def __str__(self):
         return self.species
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'plant_id': self.id})
+
+
+TYPES = (
+    ('C', "Culinary"),
+    ('H', "Homeopathic"),
+    ('T', "Topical"),
+    ('A', "Aromatic"),
+    ('M', "Medicinal"),
+)
+
+
+class Use(models.Model):
+    primary_type = models.CharField(
+        max_length=1,
+        choices=TYPES
+    )
+    description = models.CharField(max_length=480, default="")
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    def __str__(self):
+        return (f'{self.description} is a plant used for {self.get_primary_type_display()} purposes')
